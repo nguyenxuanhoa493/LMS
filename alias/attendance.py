66f4d4881b0667108b033934dba0d8597bc0c056
xuanhoa493 <nguyenxuanhoa493@gmail.com>
2023-11-29 01:15:23 +0700

    fix pass_master: move arg type_school to index 3

--- alias/attendance.py
+++ alias/attendance.py
@@ -1,4 +1,4 @@
-import sys, os, re
+import sys, os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ""))
 from API import lms
--- alias/pass_master.py
+++ alias/pass_master.py
@@ -5,8 +5,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ""))
 from API import lms
 from API.until import get_value
 
-type_dmn = get_value(sys.argv, key=1)
-dmn = sys.argv[2]
+dmn = sys.argv[1]
+type_dmn = get_value(sys.argv, key=2, default="alpha")
 new_pass_master = get_value(sys.argv, key=3)
 school = lms.New(type_dmn=type_dmn, dmn=dmn, user_code="root")
 school.set_pass_master(new_pass_master) if new_pass_master else school.get_pass_master()
